b24f308749356e6d9d85e640989b86bf369cd659
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2025-12-28 17:17:11 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+# quantum_sim/__init__.py
 
+# This file marks 'quantum_sim' as a Python package.
+# It can also be used to selectively import core components for easier access.
+# from .core.circuit import QuantumCircuit
+# from .core.parameter import Parameter
+# from .gates.single_qubit_gates import Hadamard
+# from .backends.numpy_backend import NumpyBackend
