eb34ebe75c9094ccfdb3199035a8b1a15dd6f41f
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2025-12-29 00:24:02 -0600

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,9 @@
 from .circuit import QuantumCircuit
 from .register import Register
 from .parameter import Parameter
+class QuantumCircuit:
+    def __init__(self, num_qubits: int, name: str = "Circuit"):
+        self.num_qubits = num_qubits
+        self.name = name
+        self.operations = []
+        self._components = [] # Ensure this exists if backends call it
