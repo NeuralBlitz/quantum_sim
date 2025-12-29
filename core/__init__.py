3a394ef95a469030ad5f3aecbf6a237c5324b84a
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2025-12-28 20:22:59 -0600

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,5 @@
-# quantum_sim/core/__init__.py
+from .register import Register
+from .parameter import Parameter
+from .circuit import QuantumCircuit
 
-# This file marks 'core' as a Python package.
-# It can also be used to selectively import core components for easier access.
-# from .qubit import Qubit
-# from .register import Register
-# from .parameter import Parameter
-# from .circuit import QuantumCircuit, CircuitComponent, GateOperation
-# from .noise import NoiseChannel, DepolarizingChannel, ThermalRelaxationChannel
+__all__ = ["Register", "Parameter", "QuantumCircuit"]
