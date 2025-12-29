3352651e960a03448d983d74e2799095ec3fafed
David Riggs <79354388+NeuralBlitz@users.noreply.github.com>
2025-12-28 23:57:26 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,1 @@
-# quantum_sim/__init__.py
 
-"""
-Quantum Simulation Engine
-A high-performance density matrix simulator for NISQ-era algorithms.
-"""
-
-from .core.circuit import QuantumCircuit
-from .core.parameter import Parameter
-from .backend.numpy_backend import NumpyBackend
-
-__all__ = [
-    "QuantumCircuit",
-    "Parameter",
-    "NumpyBackend",
-]
